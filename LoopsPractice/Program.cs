32b3b48984634846b13cda01bbc64248d88f7835
mzdixon1 <mzdixon1@yahoo.com>
2017-07-06 11:14:59 -0400

    Finished For Loop example

--- LoopsPractice/Program.cs
+++ LoopsPractice/Program.cs
@@ -26,7 +26,7 @@ namespace LoopsPractice
             //Foreach Loops
             //While Loops
             //Do-while loops
-            
+
             //for (int i = 15; i > 0; i--) 
             //{
             //    Console.WriteLine(i);
@@ -37,10 +37,39 @@ namespace LoopsPractice
             //    Console.WriteLine(counter);
             //}
 
-            for(int i = 50; i<= 100; i++)
+            //string greeting = "My name is Little Bill";
+            ////The .Split() method takes a string and turns it into an array of smaller strings (substrings). It defaults the split on splaces.
+            //string[] wordsInGreeting = greeting.Split();
+
+            ////for loop tp print the words
+            //for(int i = 0; i < wordsInGreeting.Length; i++)
+            //{
+            //    Console.WriteLine(wordsInGreeting[i]);
+            //      }
+            //Create an array called days with the elements Monday, Tuesday, Wednesday.
+            //Using a For Loop, print the elements of the days array.
+            //string[] days = { "Monday", "Tuesday", "Wednesday" };
+
+            //for(int i = 0; i < days.Length; i++)
+            //{
+            //    Console.WriteLine(days[i]);
+            //}
+            //Start with the string "Once Upon a time" and create an array called storyWords using the Split method.  Reverse the order of the elements in the array and using a For Loop, print each word.
+            string storyWords = "Once Upon a time";
+            string[] wordsInStory = storyWords.Split();
+            Array.Reverse(wordsInStory);
+            for (int i = 0; i < wordsInStory.Length; i++)
+            {
+              Console.WriteLine(wordsInStory[i]);
+            }
+
+            //Add the product (multiplication) of 1 through 10 using a For Loop.
+            for (int i = 0; i <= 10; i++)
             {
                 Console.WriteLine(i);
             }
+            //Print the product to the screen.
+
 
 
         }
